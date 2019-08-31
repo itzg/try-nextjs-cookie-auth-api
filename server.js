818fc42372bbd95b41cd0edf5b4137d506df8cc1
Geoff Bourne <itzgeoff@gmail.com>
2019-08-31 10:52:09 -0500

    Use all as the fallback express route

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const handle = app.getRequestHandler()
 app.prepare().then(() => {
   const server = express();
 
-  server.get('*', (req, res) => {
+  server.all('*', (req, res) => {
     return handle(req, res);
   });
 
