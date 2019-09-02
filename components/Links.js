4f45dc4b7b5d0e31410bd19864a518711517b9f5
Geoff Bourne <itzgeoff@gmail.com>
2019-09-02 17:39:34 -0500

    Disable navigation link for the current route

--- components/Links.js
+++ components/Links.js
@@ -1,22 +1,42 @@
 import Link from 'next/link';
+import {useRouter} from "next/router";
+
+function RouterAwareLink({ href, title }) {
+  const router = useRouter();
+
+  return (
+      <div className="root">
+
+        <Link href={href}>
+          { router.pathname == href ?
+            <span>{title}</span> :
+            <a>{title}</a>
+          }
+        </Link>
+
+        {/*language=css*/}
+        <style jsx>{`
+          .root {
+            display: inline-block;
+          }
+          .root + .root {
+            margin-left: 5px;
+          }
+        `}</style>
+      </div>
+  )
+}
 
 function Links() {
   return (
       <div className="root">
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
+        <RouterAwareLink href="/" title="Home"/>
+        <RouterAwareLink href="/about" title="About"/>
 
+        {/*language=css*/}
         <style jsx>{`
           .root {
             margin-top: 20px;
-          }
-          
-          a + a {
-            margin-left: 5px;
           }
         `}</style>
       </div>
